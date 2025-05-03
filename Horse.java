06d6c620a2f827ab09f29c089b801b9c93d6c0c1
harrisaffandi <>
2025-05-03 21:48:49 +0100

    Added a testing method in Horse.java

--- Horse.java
+++ Horse.java
@@ -91,6 +91,38 @@ public class Horse
         this.symbol = newSymbol;
     }
 
+    //testing
+    public static void main(String[] args) {
+
+        //horse 1
+        Horse horse1 = new Horse('A', "Harris", -1);
+        System.out.println("Horse 1 Synbol: " + horse1.getSymbol());
+        System.out.println("Horse 1 name: " + horse1.getName());
+        System.out.println("Horse 1 confidence: "+horse1.getConfidence());
+        System.out.println("Horse 1 distance travelled: "+horse1.getDistanceTravelled());
+        horse1.moveForward();
+        System.out.println("Horse 1 new distance travelled: " +horse1.getDistanceTravelled());
+        System.out.println("Horse 1 fallen status: " + horse1.hasFallen());
+
+        //horse 2
+        Horse horse2 = new Horse('B', "Affandi", 1.2);
+        System.out.println("Horse 2 Synbol: " +horse2.getSymbol());
+        System.out.println("Horse 2 name: " +horse2.getName());
+        System.out.println("Horse 2 confidence: "+horse2.getConfidence());
+        System.out.println("Horse 2 distance travelled: "+horse2.getDistanceTravelled());
+        horse2.moveForward();
+        System.out.println("Horse 2 new distance travelled: " +horse2.getDistanceTravelled());
+        System.out.println("Horse 2 fallen status: " + horse2.hasFallen());
+        horse2.fall();
+        System.out.println("Horse 2 fallen status: " + horse2.hasFallen());
+
+        //check if confidence can exceed the bounds
+        horse2.setConfidence(1.5);
+        System.out.println("Horse 2 new confidence: " + horse2.getConfidence()); //should return 0.9
+        horse2.setConfidence(-0.5);
+        System.out.println("Horse 2 new confidence: " + horse2.getConfidence()); //should return 0.1
+    }
+
 
     
 }
